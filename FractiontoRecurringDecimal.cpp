121aa82cec827db522c090bff996d73736950546
Mengyu Chung <mengyuch@usc.edu>
2016-03-19 11:20:23 -0700

    Improve FractiontoRecurringDecimal.cpp
    Make it more concise.

--- FractiontoRecurringDecimal.cpp
+++ FractiontoRecurringDecimal.cpp
@@ -1,65 +1,51 @@
 class Solution {
 public:
+    // Reference:
+    // https://leetcode.com/discuss/18731/accepted-cpp-solution-with-explainations
     string fractionToDecimal(int numerator, int denominator) {
         string result;
         unordered_map<int, int> map;
-        bool firstTime = true; // first time n < d, have to put dot
-        bool neg = false;
         long n = numerator, d = denominator; // prevent error caused by INT_MIN
         
+        // numerator is zero
         if (!n) {
             return "0";
         }
-        // check sign
-        if (n < 0) {
-            n = abs(n);
-            neg = !neg;
-        }
-        if (d < 0) {
-            d = abs(d);
-            neg = !neg;
-        }
-        // concise way to check sign
-        // if (n < 0 ^ d < 0)
 
-        while (n != 0) {
-            if (n < d) {
-                if (firstTime) {
-                    if (result.empty()) {
-                        result += "0";
-                    }
-                    result += ".";
-                    firstTime = false;
-                } else {
-                    if (map.find(n) == map.end()) {
-                        map[n] = result.size();
-                    } else {
-                        result.insert(result.begin() + map.at(n), '(');
-                        result += ')';
-                        return result;
-                    }
-                    result += "0";
-                }
-                n *= 10;
-            } else {
-                if (map.find(n) == map.end()) {
-                    map[n] = result.size();
-                } else {
-                    result.insert(result.begin() + map.at(n), '(');
-                    result += ')';
-                    break;
-                }
-                result += to_string(n / d);
-                n %= d;
-                if (n != 0) {
-                    n *= 10;
-                    if (firstTime) {
-                        result += ".";
-                        firstTime = false;
-                    }
-                }
-            }
+        // concise way to check sign
+        if (n < 0 ^ d < 0) {
+            result += '-';
         }
-        return neg ? "-" + result : result;
+
+        // eliminate negative sign
+        n = abs(n), d = abs(d);
+        
+        // append integral part
+        result += to_string(n / d);
+        
+        if (n % d == 0) {
+            return result;
+        }
+        
+        // fraction part starts
+        result += '.';
+        n = n % d;
+        
+        while (n != 0) {
+            n *= 10;
+            
+            // if we meet this remainder before
+            if (map.find(n) == map.end()) {
+                map[n] = result.size();
+            } else {
+                result.insert(result.begin() + map.at(n), '(');
+                result += ')';
+                break;
+            }
+
+            result += to_string(n / d);
+            n %= d;
+        }
+        return result;
     }
 };
