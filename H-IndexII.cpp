c0913c268e6769dc9f4c9332040e091b4b0315c0
Mengyu Chung <mengyuch@usc.edu>
2016-05-25 14:03:25 -0700

    Make it concise

--- H-IndexII.cpp
+++ H-IndexII.cpp
@@ -3,20 +3,15 @@ public:
     int hIndex(vector<int>& citations) {
         int len = citations.size();
         int lo = 0, hi = len - 1, mi;
-        
-        if (len == 0) return 0;
-        while (lo < hi) {
+
+        while (lo <= hi) {
             mi = lo + (hi - lo) / 2;
             if (citations[mi] < len - mi) {
                 lo = mi + 1;
             } else {
-                hi = mi;
+                hi = mi - 1;
             }
         }
-        if (citations[lo] < len - lo) {
-            return len - lo - 1;
-        } else {
-            return len - lo;
-        }
+        return len - lo;
     }
 };
