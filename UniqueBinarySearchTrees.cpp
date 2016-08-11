c6ba1438b83c6e8a22dfc06aedba75bb115b0eaf
Mengyu Chung <mengyuch@usc.edu>
2016-08-11 10:42:07 -0700

    Update

--- UniqueBinarySearchTrees.cpp
+++ UniqueBinarySearchTrees.cpp
@@ -7,7 +7,7 @@ public:
         G[0] = G[1] = 1;
         
         for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= i; j++) {
                 G[i] += G[j-1] * G[i-j];
             }
         }
